a82a646eeb350c33a95b7ca769af1bf761e8c875
Vaslav <2key@rambler.ru>
2025-01-15 19:19:19 +0500

    first code

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-bot = Bot(token='')
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
